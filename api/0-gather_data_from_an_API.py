81679eb0bb64da39df6e8cced5cd1eef4933b6a0
aadedigbae <a.adedigba@alustudent.com>
2023-11-05 21:56:16 +0000

    files

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     employee_id = sys.argv[1]
     user_url = "https://jsonplaceholder.typicode.com/users/{}" \
         .format(employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos" \
         .format(employee_id)
 
     user_info = requests.request('GET', user_url).json()
@@ -27,4 +27,4 @@ if __name__ == '__main__':
           format(employee_name, number_of_done_tasks, total_number_of_tasks))
 
     [print("\t " + task["title"]) for task in task_completed]
-    
+
